6467cdc0b8de54f81bceaeab32eff6809f3b3a85
Jilles van Gurp <jilles@jillesvangurp.com>
2016-01-25 22:43:39 +0100

    make builder a nested class, there's no need to access that directly

--- src/main/java/io/inbot/elasticsearch/crud/CrudOpererationsFactory.java
+++ src/main/java/io/inbot/elasticsearch/crud/CrudOpererationsFactory.java
@@ -4,6 +4,7 @@ import com.github.jsonj.tools.JsonParser;
 import io.inbot.elasticsearch.client.ElasticSearchType;
 import io.inbot.elasticsearch.client.EsAPIClient;
 import io.inbot.redis.RedisBackedCircularStack;
+import io.inbot.redis.RedisCache;
 import redis.clients.jedis.JedisPool;
 
 public class CrudOpererationsFactory {
@@ -21,5 +22,85 @@ public class CrudOpererationsFactory {
 
     public CrudOperationsBuilder builder(ElasticSearchType type) {
         return new CrudOperationsBuilder(esApiClient, parser, jedisPool, redisBackedCircularStack, type);
+    }
+
+    public static class CrudOperationsBuilder {
+        private final ElasticSearchType indexType;
+        private final EsAPIClient esApiClient;
+
+        private final JsonParser parser;
+        private final JedisPool jedisPool;
+        private final RedisBackedCircularStack redisBackedCircularStack;
+        private int retryUpdates = 5;
+        private boolean redis = false;
+        private int redisExpireAfterWriteInSeconds = 10;
+        private String redisPrefix = "none";
+        private boolean inMemoryCache = false;
+        private int maxInMemoryItems;
+        private int inMemoryExpireAfterWriteSeconds;
+
+        /**
+         * Don't call this directly and use the CrudOperationsFactory instead.
+         */
+        CrudOperationsBuilder(EsAPIClient esApiClient, JsonParser parser, JedisPool jedisPool, RedisBackedCircularStack redisBackedCircularStack,
+                ElasticSearchType indexType) {
+            this.esApiClient = esApiClient;
+            this.parser = parser;
+            this.jedisPool = jedisPool;
+            this.redisBackedCircularStack = redisBackedCircularStack;
+            this.indexType = indexType;
+        }
+
+        public CrudOperationsBuilder retryUpdates(int times) {
+            retryUpdates = times;
+            return this;
+        }
+
+        public CrudOperationsBuilder enableRedisCache(boolean enableRedis, int expireAfterWriteInSeconds, String prefix) {
+            if(enableRedis) {
+                this.redisExpireAfterWriteInSeconds = expireAfterWriteInSeconds;
+                this.redisPrefix = prefix;
+                redis = true;
+            }
+            return this;
+        }
+
+        public CrudOperationsBuilder enableInMemoryCache(int maxItems, int expireAfterWriteSeconds) {
+            this.maxInMemoryItems = maxItems;
+            this.inMemoryExpireAfterWriteSeconds = expireAfterWriteSeconds;
+            inMemoryCache = true;
+            return this;
+        }
+
+        public CrudOperations dao() {
+            if(indexType.parentChild()) {
+                throw new IllegalStateException("type " + indexType + " specifies parent child relations");
+            }
+            CrudOperations dao = new EsCrudDao(indexType, esApiClient, redisBackedCircularStack, retryUpdates);
+            if(redis) {
+                dao = new RedisCachingCrudDao(dao, new RedisCache(jedisPool, parser, redisPrefix, indexType.version(), redisExpireAfterWriteInSeconds));
+            }
+            if(inMemoryCache) {
+                dao = new GuavaCachingCrudDao(dao, maxInMemoryItems, inMemoryExpireAfterWriteSeconds);
+            }
+
+            return dao;
+        }
+
+        public ParentChildCrudOperations childDao() {
+            if(!indexType.parentChild()) {
+                throw new IllegalStateException("type " + indexType + " does not specify parent child relations");
+            }
+
+            ParentChildCrudOperations dao = new EsParentChildCrudDao(indexType, esApiClient, redisBackedCircularStack, retryUpdates);
+            if(redis) {
+                dao = new RedisCachingParentChildCrudDao(dao, new RedisCache(jedisPool, parser, redisPrefix, indexType.version(), redisExpireAfterWriteInSeconds));
+            }
+            if(inMemoryCache) {
+                dao = new GuavaCachingChildCrudDao(dao, maxInMemoryItems, inMemoryExpireAfterWriteSeconds);
+            }
+
+            return dao;
+        }
     }
 }
